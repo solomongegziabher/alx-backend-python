cf04eb99fe8e5c92fa08078bf4f1062d90810da5
Solomon <113886983+solomongegziabher@users.noreply.github.com>
2023-08-03 23:06:13 +0300

    Delete 0-add.py

--- 0-add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Module that contains a single function that adds two integers."""
-
-
-def add(a: float, b: float) -> float:
-    """ Add two integers together."""
-    return a + b
